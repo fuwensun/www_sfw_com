e92bea1a9b69634ec3a36d2ae8e2a743b38cf96d
sfw <sfuwen@sina.com>
2018-03-31 01:49:48 +0800

    ---create model---

--- mian.py
+++ mian.py
@@ -10,6 +10,18 @@ db = SQLAlchemy(app)
 def hello_world():
     return 'Hello World!'
 
+class User(db.Model):
+    __tablename__ = "user_table_name"
+    id = db.Column(db.Integer(),primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self,username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
 
 if __name__ == '__main__':
     app.run()
