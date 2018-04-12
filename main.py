c55181f8a301fe64b3d914b212299f6738a22b82
sfw <sfuwen@sina.com>
2018-04-12 22:03:51 +0800

    ---add blueprint---

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint, redirect, url_for
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
@@ -21,6 +21,13 @@ tags = db.Table(
     'post_tags',
     db.Column('post_id', db.Integer(), db.ForeignKey('post.id')),
     db.Column('tag_id',db.Integer(),db.ForeignKey('tag.id')),
+)
+
+blog_blueprint = Blueprint(
+    'blog',
+    __name__,
+    template_folder = 'templates/blog',
+    url_prefix = "/blog"
 )
 
 class User(db.Model):
@@ -89,19 +96,6 @@ class Tag(db.Model):
         return "<Tag '{}'>".format(self.title)
 
 
-# def sidebar_data():
-#     recent = Post.query.order_by(
-#         Post.publish_data.desc()
-#     ).limit(5).all()
-#
-#     top_tags = db.session.query(
-#         Tag, func.count(tags.c.post_id).label('total')
-#     ).join(
-#         tags
-#     ).group_by(Tag).order_by('total DESC').limit(5).all()
-#
-#     return recent, top_tags
-
 class CommentForm(FlaskForm):
     name = StringField(
         'Name',
@@ -109,13 +103,6 @@ class CommentForm(FlaskForm):
     )
     text = TextAreaField(u'Comment', validators=[DataRequired()])
 
-# def sidebar_data():
-#     recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
-#     top_tags = db.session.query(
-#         Tag, func.count(tags.c.post_id).label('total')
-#     ).join(tags).group_by(Tag).order_by('total').limit(5).all()
-#
-#     return recent, top_tags
 
 def sidebar_data():
     recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
@@ -125,8 +112,15 @@ def sidebar_data():
 
     return recent, top_tags
 
+
 @app.route('/')
-@app.route('/<int:page>')
+def index():
+    return redirect(url_for('blog.home'))
+
+
+
+@blog_blueprint.route('/')
+@blog_blueprint.route('/<int:page>')
 def home(page=1):
     posts = Post.query.order_by(
         Post.publish_date.desc()
@@ -135,29 +129,15 @@ def home(page=1):
     recent, top_tags = sidebar_data()
 
     return render_template(
-        'home.html',                    #   html to render
+        'blog/home.html',                    #   html to render
         posts = posts,                  #   some var to html
         recent = recent,
         top_tags = top_tags
     )
 
-# @app.route('/post/<int:post_id>')
-# def post(post_id):
-#     post = Post.query.get_or_404(post_id)
-#     tags = post.tags
-#     comments = post.comments.order_by(Comment.date.desc()).all()
-#     recent, top_tags = sidebar_data()
-#
-#     return render_template(
-#         'post.html',
-#         post = post,
-#         tags = tags,
-#         comments = comments,
-#         recent = recent,
-#         top_tags = top_tags
-#     )
 
-@app.route('/post/<int:post_id>', methods=('GET', 'POST'))
+
+@blog_blueprint.route('/post/<int:post_id>', methods=('GET', 'POST'))
 def post(post_id):
     form = CommentForm()
     if form.validate_on_submit():       #new comment submit
@@ -176,7 +156,7 @@ def post(post_id):
     recent, top_tags = sidebar_data()
 
     return render_template(
-        'post.html',
+        'blog/post.html',
         post=post,
         tags=tags,
         comments=comments,
@@ -185,32 +165,35 @@ def post(post_id):
         form=form
     )
 
-@app.route('/tag/<string:tag_name>')
+@blog_blueprint.route('/tag/<string:tag_name>')
 def tag(tag_name):
     tag = Tag.query.filter_by(title = tag_name).first_or_404()
     posts = tag.posts.order_by(Post.publish_date.desc()).all()
     recent, top_tags = sidebar_data()
 
     return render_template(
-        'tag.html',
+        'blog/tag.html',
         tag = tag,
         posts = posts,
         recent = recent,
         top_tags = top_tags
     )
 
-@app.route('/user/<string:username>')
+@blog_blueprint.route('/user/<string:username>')
 def user(username):
     user = User.query.filter_by(username = username).first_or_404()
     posts = user.posts.order_by(Post.publish_date.desc()).all()
     recent, top_tags = sidebar_data()
 
     return render_template(
-        'user.html',
+        'blog/user.html',
         user = user,
         posts = posts,
         recent = recent,
         top_tags = top_tags
     )
+
+app.register_blueprint(blog_blueprint)
+
 if __name__ == '__main__':
     app.run()
