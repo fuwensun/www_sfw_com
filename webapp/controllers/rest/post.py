f86b0f6c974304bc0753366612d52198c23a21b8
sfw <sfuwen@sina.com>
2018-04-15 20:42:07 +0800

    ---add restful HTMLParser---

--- webapp/controllers/rest/post.py
+++ webapp/controllers/rest/post.py
@@ -1,7 +1,22 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
+from flask import abort
+from webapp.models import Post
+from .fields import HTMLField
+
+post_fields = {
+    'title': fields.String(),
+    'text':HTMLField(),
+    'publish_date':fields.DateTime(dt_format='iso8601')
+}
 
 class PostApi(Resource):
+    @marshal_with(post_fields)
     def get(self,post_id=None):
         if post_id:
-            return{'id':post_id}
-        return {'hello':'world'}
+            post = Post.query.get(post_id)
+            if not post:
+                abort(404)
+            return post
+        else:
+            posts = Post.query.all()
+            return posts
